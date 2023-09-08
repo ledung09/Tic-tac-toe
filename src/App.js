6ab6cbec10da74f5df2f4fbd45ad7b5fbc707beb
Le Dung <dp09.it@gmail.com>
2023-09-08 13:46:33 +0700

    Add select player

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import './gameboard/board.css';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <Row className='cont-r1'>
+        <Col 
+          className="wrapper wrapper1"
+          xs={8}
         >
-          Learn React
-        </a>
-      </header>
+          <Navbar expand="lg">
+            <Navbar.Collapse id="navbar-dark-example">
+              <Nav>
+                <NavDropdown
+                  id="nav-dropdown-dark-example"
+                  title="Select play mode "
+                >
+                  <NavDropdown.Item href="#action/3.1">Play vs AI</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">2 Player Mode</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+        </Col>
+      </Row>
+       <Row className='cont-r2'>
+        <Col 
+          className="wrapper wrapper2"
+          xs={8}
+        ></Col>
+      </Row>
     </div>
   );
 }
